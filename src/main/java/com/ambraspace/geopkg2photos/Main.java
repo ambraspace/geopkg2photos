1dd543c7f26856bc904d73e44a5655b9246ac591
Branimir Amidžić <mail@ambraspace.com>
2020-08-03 14:20:29 +0200

    Basic code added

---
+++ src/main/java/com/ambraspace/geopkg2photos/Main.java
@@ -0,0 +1,62 @@
+package com.ambraspace.geopkg2photos;
+
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main
+{
+	
+	
+	public static void main(String[] args)
+	{
+		for (String s:args)
+		{
+			System.out.println("Processing file " + s);
+			connectAndExtract(s);
+		}
+	}
+	
+	
+	private static void connectAndExtract(String fileName)
+	{
+		
+		Connection c = null;
+
+		try {
+
+			Class.forName("org.sqlite.JDBC");
+	
+			c = DriverManager.getConnection("jdbc:sqlite:" + fileName);
+
+			Statement query = c.createStatement();
+			ResultSet rs = query.executeQuery("SELECT * FROM Photos");
+			String imName;
+			byte[] im;
+			while (rs.next())
+			{
+				imName = rs.getString("UUID");
+				im = rs.getBytes("photo");
+				System.out.println("Saving " + imName + ".jpg");
+				FileOutputStream fos = new FileOutputStream(imName + ".jpg");
+				fos.write(im);
+				fos.close();
+			}
+			rs.close();
+			query.close();
+			c.close();
+			
+		} catch ( Exception e ) {
+
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	
+			return;
+
+		}
+		
+
+	}
+
+}
